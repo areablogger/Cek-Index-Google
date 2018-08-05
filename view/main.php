f0d258202cd3bce410bad6d504d48e415a88eeca
Admin Areablogger <idblogpedia@gmail.com>
2018-08-05 15:16:24 +0700

    Delete main.php

--- view/main.php
+++
@@ -1,8 +0,0 @@
-<?php echo form_open('moz/grabdata');?>
-<input type="url" name="url" placeholder="url"/>
-<input type="submit" name="submit" value="Submit"/>
-</form>
-
-<?php
-print_r($datamoz);
-?>
